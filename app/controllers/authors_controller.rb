45b1841452b5af6a3ff8f2b0c41d1344296bf533
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-10-20 22:01:32 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,32 @@
 class AuthorsController < ApplicationController
-  def show
-    @author = Author.find(params[:id])
-  end
 
   def new
     @author = Author.new
   end
-
+  
+  def show
+    @author = Author.find(params[:id])
+  end
+  
   def create
     @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    if @author.valid? 
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  
   private
 
   def author_params
     params.permit(:name, :email, :phone_number)
   end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,45 @@
 class PostsController < ApplicationController
-  def show
+   def show
     @post = Post.find(params[:id])
-  end
+  end 
 
-  def edit
+ def edit
     @post = Post.find(params[:id])
   end
 
   def update
     @post = Post.find(params[:id])
-
     @post.update(post_params)
-
+    if @post.valid?
     redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
+ 
+#  def create
+#     @post = Post.create(post_params)
+#     if @post.valid? 
+#       @post.save
+#       redirect_to post_path(@post)
+#     else
+#       render :new
+#     end
+#   end
+  
 
+
+ 
+
+    
+ 
+
+ 
+  
   private
 
   def post_params
     params.permit(:title, :category, :content)
   end
+
 end

bcc8d527f21f332c71df146efeb7e2b97e1edd89
Newland-A <ajn252@gmail.com>
2020-10-27 05:42:21 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates_length_of :content, minimum: 100
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  
 end
